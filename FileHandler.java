ba2db318fc7ff3cc2ff1c79c28af4000c9cff6d1
Ben Stumpf <benlstumpf@gmail.com>
2020-01-30 16:42:47 -0600

    javadoc documentation

--- FileHandler.java
+++ FileHandler.java
@@ -1,17 +1,22 @@
 public class FileHandler{
 
-
-  public static BufferedReader getReader(String filePath){
+  /**
+   * Create a bufferreader and prequeisit objects from a file location string
+   * @param  filePath file location path
+   * @return          BufferReader needed to read a file
+   */
+  public static BufferedReader getBufferReader(String filePath){
 
 
     return new BufferedReader(fileReader);
   }
 
-
-
-
+  /**
+   * Closes an open buffer reader
+   * @param reader The bufferreader to be closed
+   */
   public static void closeReader(BufferedReader reader){
-
+    reader.close();
   }
 
 }
