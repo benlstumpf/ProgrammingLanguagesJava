d5272d80f290421412c13bce0dd3a9103129821b
Ben Stumpf <benlstumpf@gmail.com>
2020-01-31 14:38:59 -0600

    paste of design doc for ease of coding

--- DataCleaning.java
+++ DataCleaning.java
@@ -1,3 +1,21 @@
+/*
+•	Data cleaning Class
+o	Data cleaning:
+	Chooses the correct function to call in cleaning words. Function can call:
+•	Split word cleaning
+•	Punctuation Removal
+•	Case normalization
+o	Split word cleaning:
+	For when words are broken at the end of a line, this function gets the next ‘word’, concatenates them and returns the word.
+o	Punctuation Removal:
+	Find and removes all punctuation
+o	Case normalization:
+	Normalizes word case to lower case.
+o	Count word:
+	Tracks words and counts them, stored in a hash map
 
+ */
 
-public
+public class DataCleaning {
+
+}
