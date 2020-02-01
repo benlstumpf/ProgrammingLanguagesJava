45473fe18bcd8350664e8139167b2575b49daf07
Ben Stumpf <benlstumpf@gmail.com>
2020-02-01 11:54:51 -0600

    tested things around hashmaps

--- SelectMethodTesting.java
+++ SelectMethodTesting.java
@@ -1,10 +1,13 @@
 
-
+import java.util.HashMap;
+import java.lang.Integer;
 
 public class SelectMethodTesting {
   public static void main (String[] args){
-    Hashmap hashmap = new Hashmap();
+    HashMap hashmap = new HashMap();
     hashmap.put("one", 1);
-    hashmap.get("one");
+    System.out.println();
+    System.out.println(hashmap.get("one").getClass().getName());
+    System.out.println();
   }
 }
