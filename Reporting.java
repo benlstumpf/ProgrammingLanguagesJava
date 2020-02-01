6085cce9cd450617313cf9a1127ce8dd01ee815d
Ben Stumpf <benlstumpf@gmail.com>
2020-02-01 17:34:34 -0600

    init objects needed for reporting

--- Reporting.java
+++ Reporting.java
@@ -2,7 +2,13 @@
 
 
 public class Reporting {
-  public static alphabitize (List list){
-    
+  public static EntryObject[] mapToSortedList (HashMap map){
+    EntryObject[] sortedList = EntryObject[map.size()];
+    EntryObject[] unsortList = EntryObject[map.size()];
+    Object[] keyList = map.keySet().toArray();
+
+
+
+    return EntryObject
   }
 }
