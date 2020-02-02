bdbce2971983f66db5a44f74a6ca213d0d4ef7bc
Ben Stumpf <benlstumpf@gmail.com>
2020-02-02 11:09:08 -0600

    trying to figure sorting, trying javas array sorting function, didnt work

--- SelectMethodTesting.java
+++ SelectMethodTesting.java
@@ -1,13 +1,26 @@
 
 import java.util.HashMap;
 import java.lang.Integer;
+import java.util.Arrays;
+
+
 
 public class SelectMethodTesting {
   public static void main (String[] args){
-    HashMap hashmap = new HashMap();
-    hashmap.put("one", 1);
-    System.out.println();
-    System.out.println(hashmap.get("one").getClass().getName());
-    System.out.println();
+    EntryObject[] array = new EntryObject[5];
+
+    array[3] = new EntryObject("one",1);
+    array[2] = new EntryObject("two",2);
+    array[4] = new EntryObject("three",3);
+    array[0] = new EntryObject("four",4);
+    array[1] = new EntryObject("five",5);
+
+    Arrays.sort(array);
+
+    System.out.println(array[0]);
+    System.out.println(array[1]);
+    System.out.println(array[2]);
+    System.out.println(array[3]);
+    System.out.println(array[4]);
   }
 }
