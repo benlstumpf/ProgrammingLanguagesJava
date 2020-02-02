2fed3af98d723d55c9ba486e976198989a46bc2b
Ben Stumpf <benlstumpf@gmail.com>
2020-02-02 16:46:16 -0600

    added function for printing arrays to the command line

--- Reporting.java
+++ Reporting.java
@@ -2,18 +2,25 @@
 
 
 public class Reporting {
-  public static EntryObject[] mapToSortedList (HashMap map){
-    EntryObject[] returningList = EntryObject[map.size()];
+  public static EntryObject[] mapToList (HashMap map){
+    EntryObject[] returningArray = EntryObject[map.size()];
     Strings[] keyArray = map.keySet().toArray(); // returns an array of keys
     Integer[] valuesArray = map.values().toArray(); // returns an array of values
-
-
-
     return returningList;
   }
-  private arraysToObjects (EntryObject[] returningList, Strings[] keyArray, Integer[] valuesArray){
-    for(int i = 0; i <keylist.length(); i++){
+  private static arraysToObjects (EntryObject[] returningList, Strings[] keyArray, Integer[] valuesArray){
+    for(int i = 0; i < keylist.length(); i++){
       sortedList[i] =  new EntryObject(keyArray[i], valuesArray[i]);
     }
   }
+  public static void arrayToFile (EntryObject[] array, BufferWriter writer){
+    for (int i = 0; i < array.length(); i++){
+      FileHandler.printRecord
+    }
+  }
+  public static void printArray(EntryObject[] array){
+    for(int i = 0; i < array.length(); i++){
+      System.out.println(array[i].toString());
+    }
+	}
 }
