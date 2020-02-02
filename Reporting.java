3fb33cd050ba959067553ce1adf717ca91ea46b2
Ben Stumpf <benlstumpf@gmail.com>
2020-02-02 17:08:22 -0600

    removed bufferreader method

--- Reporting.java
+++ Reporting.java
@@ -1,25 +1,24 @@
-
-
+import java.io.BufferedWriter;
 
 public class Reporting {
   public static EntryObject[] mapToList (HashMap map){
     EntryObject[] returningArray = EntryObject[map.size()];
-    Strings[] keyArray = map.keySet().toArray(); // returns an array of keys
+    String[] keyArray = map.keySet().toArray(); // returns an array of keys
     Integer[] valuesArray = map.values().toArray(); // returns an array of values
-    return returningList;
+    return returningArray;
   }
-  private static arraysToObjects (EntryObject[] returningList, Strings[] keyArray, Integer[] valuesArray){
-    for(int i = 0; i < keylist.length(); i++){
-      sortedList[i] =  new EntryObject(keyArray[i], valuesArray[i]);
+  public static void arraysToObjects (EntryObject[] returningList, String[] keyArray, Integer[] valuesArray){
+    for(int i = 0; i < keyArray.length; i++){
+    	returningList[i] =  new EntryObject(keyArray[i], valuesArray[i]);
     }
   }
-  public static void arrayToFile (EntryObject[] array, BufferWriter writer){
-    for (int i = 0; i < array.length(); i++){
-      FileHandler.printRecord
+  public static void arrayToFile (EntryObject[] array, BufferedWriter writer){
+    for (int i = 0; i < array.length; i++){
+      FileHandler.printRecord();
     }
   }
   public static void printArray(EntryObject[] array){
-    for(int i = 0; i < array.length(); i++){
+    for(int i = 0; i < array.length; i++){
       System.out.println(array[i].toString());
     }
 	}
