7be6c4aeb917c3e5f13aed16091ae5750feabdf9
Ben Stumpf <benlstumpf@gmail.com>
2020-02-01 18:42:30 -0600

    seters and getters

--- EntryObject.java
+++ EntryObject.java
@@ -1,5 +1,21 @@
 
-Public class EntryEntryObject{
+public class EntryObject{
   private String key;
-  Integer value = 0;
+  private Integer value = 0;
+
+  public Integer getValue (){
+    return value;
+  }
+
+  public void setValue (Integer i){
+    value = i;
+  }
+
+  public String getKey (){
+    return key;
+  }
+
+  public void setKey (String s){
+    key = s;
+  }
 }
