b51b841fb419f0bbf0f6dd2cfdcd440aa86dd0b0
Ben Stumpf <benlstumpf@gmail.com>
2020-02-02 16:47:48 -0600

    fixed call

--- CLInterface.java
+++ CLInterface.java
@@ -9,15 +9,20 @@ import java.util.Hashmap;
 
 Class CLInterface {
 	public static void main (String[] args){
+		//get file in scanner so file can be read
 		String filePath = args[0];
 		Scanner scanner = FileHandler.getScanner(filePath);
+
+		//Make HashMap to store words
 		Hashmap hashmap = new Hashmap();
 		hashmap = WordCounting.countwords(scanner, hashmap);
 
-		EntryObject[] completeList = Reporting.mapToSortedList(hashmap);
-		EntryObject[] topFive = Reporting.getTopFive(completeList);
-		EntryObject[] bottomFive = Reporting.getBottomFive(completeList);
+		//Convert hashmap from map to array
+		EntryObject[] completeList = Reporting.mapToList(hashmap);
+		//EntryObject[] topFive = Reporting.getTopFive(completeList);
+		//EntryObject[] bottomFive = Reporting.getBottomFive(completeList);
 
+		Reporting.printArray()
 	}
 
 }
