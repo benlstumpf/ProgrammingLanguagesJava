783940dd039eac885d792cf1ae3fe85354d1b836
Ben Stumpf <benlstumpf@gmail.com>
2020-02-02 20:46:59 -0600

    remove puntuation

--- DataCleaning.java
+++ DataCleaning.java
@@ -17,7 +17,10 @@ o	Count word:
  */
 
 public class DataCleaning {
-
+  public static String cleanWord (String word){
+    //end of sentance puncutation
+    word = word.replaceAll(\p{Punct}, "");
+  }
 
 
 
