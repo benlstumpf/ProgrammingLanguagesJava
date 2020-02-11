ba0570ac96d43a3fd08dc9153aeee93eb9407a8b
Ben Stumpf <benlstumpf@gmail.com>
2020-02-10 18:02:07 -0600

    make a window

--- GuiElements.java
+++ GuiElements.java
@@ -1,5 +1,20 @@
 
 
-class Guielements{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+class GuiElements extends JFrame implements ActionListener{
+  public GuiElements() {
+      this.setTitle("Word Counting Application");
+      this.setBounds( 200, 300, 400, 300);
+      this.getContentPane().setLayout(null);
+      this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+  }
 }
